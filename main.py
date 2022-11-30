1be12cd16e9d1e7c7ab02513e1c83bb9b1b0b30f
vanten-s <svante.spannar@gmail.com>
2022-11-30 17:20:39 +0100

    Layer Class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import math
+import random
 
 def ReLU(x):
     return (math.abs(x)+x)/2
@@ -21,8 +22,23 @@ class Node:
 
         return self.activation_func(sum([inputs[i] * self.weights[i] for i in range(0, len(inputs))])+bias)
 
+class Layer:
+    def __init__(self, n_nodes, n_inputs, weights, biases, activation_func):
+        self.n_nodes = n_nodes
+        self.n_inputs = n_inputs
+        self.weights = weights
+        self.biases = biases
+        self.activation_func = activation_func
+        self.nodes = [Node(n_inputs, weights[i], biases[i], activation_func) for i in range(0, len(n_nodes))]
+
+    def randomise_factors(self, radius):
+        for node in self.nodes:
+            node.bias += (2*random.random()-1) * radius
+            for i in range(0, len(node.weights)):
+                node.weights[i] += (2*random.random()-1) * radius
 
 
-
+    def get_output(inputs):
+        return [node.get_output(inputs) for node in self.nodes]
 
 
